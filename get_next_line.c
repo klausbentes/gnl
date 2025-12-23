6fc7cfe82deac7fc412723b052fdf5340583b233
Klaus Bentes <klausbentes@gmail.com>
2025-12-23 19:25:33 -0300

    Working functions

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kbentes- <kbentes-@student.42.rio>         +#+  +:+       +#+        */
+/*   By: kbentes- <kbentes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 16:52:03 by kbentes-          #+#    #+#             */
-/*   Updated: 2025/12/23 16:52:05 by kbentes-         ###   ########.fr       */
+/*   Updated: 2025/12/23 19:25:06 by kbentes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_has_new_line(char *string)
 	int	i;
 
 	if (!string)
-		return (NULL);
+		return (0);
 	i = 0;
 	while (string[i] != '\0' && string[i] != '\n')
 		i++;
@@ -26,27 +26,56 @@ int	ft_has_new_line(char *string)
 	return (0);
 }
 
+static char	*copy_until_new_line(char *line, const char *buf)
+{
+	size_t	len_line;
+	size_t	len_buf;
+	size_t	i;
+	char	*new_string;
+
+	if (!line || !buf)
+		return (NULL);
+	len_line = ft_strlen(line);
+	len_buf = 0;
+	while (buf[len_buf] && buf[len_buf] != '\n')
+		len_buf++;
+	new_string = ft_calloc(len_line + len_buf + 2);
+	i = 0;
+	while (line[i] != '\0')
+		new_string[i] = line[i++];
+	i = 0;
+	while (buf[i] && buf[i] != '\n')
+		new_string[len_line + i] = buf[i++];
+	new_string[len_line + i] = '\n';
+    free(line);
+	return (new_string);
+}
+
 static char *ft_read_line(int fd, char *stash)
 {
     char    *buf;
-    char        *line;
+    char    *line;
     ssize_t bytes_read;
-    
+
+    buf = ft_calloc(BUFFER_SIZE + 1, 1);
     // Se já havia algo em stash (o resto de uma chamada anterior):
+    line = NULL;
     if (stash != NULL)
     {
         *buf = stash;
         free(stash);
         bytes_read = ft_strlen(buf);
     }
-    else
+    else // primeira chamada
         bytes_read = read(fd, buf, (size_t) BUFFER_SIZE);
     // Dentro de um while, verificar se há \n no meu buf
     while (ft_has_new_line(buf) == 0 && bytes_read > 0) // leu algo e não tem new_line
     {
-        *line = ft_strjoin(line, buf);
+        line = copy_until_new_line(line, buf); // se line for NULL, line vai apontar pra uma string que é igual a buf (modificar strjoin)
         bytes_read = read(fd, buf, BUFFER_SIZE);
     }
+    // Adiciona tudo que foi lido até \n na line
+    line = copy_until_new_line(line, buf);
     // Se buf tem \n, stash = ft_strchr(buf, '\n')
     if (ft_has_new_line(buf))
         stash = ft_strdup(ft_strchr(buf, '\n') + 1);
@@ -61,7 +90,6 @@ char *get_next_line(int fd)
     // Se não houver arquivo válido para ler ou espaço de memória
     if (fd < 0 || BUFFER_SIZE <= 0)
         return (NULL);
-    // buf = NULL;
 
     return (ft_read_line(fd, stash));
 }
